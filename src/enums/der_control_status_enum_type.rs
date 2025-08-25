05dea20e02bd3fe4392963750cc74e8960c068ff
Unwize <aaronsigaldev@gmail.com>
2025-08-25 13:56:43 -0700

    Impl Default

--- src/enums/der_control_status_enum_type.rs
+++ src/enums/der_control_status_enum_type.rs
@@ -1,9 +1,10 @@
 use serde::{Deserialize, Serialize};
 use std::convert::TryFrom;
 
-#[derive(Clone, Eq, PartialEq, Debug, Serialize, Deserialize)]
+#[derive(Clone, Eq, PartialEq, Debug, Serialize, Deserialize, Default)]
 pub enum DERControlStatusEnumType {
     /// Operation successful.
+    #[default]
     Accepted,
     /// Operation failed.
     Rejected,
